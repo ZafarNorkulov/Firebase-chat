95b3c8e2d6ab33a2e34a463e2420cc88738ccd35
ZafarNorkulov <norqulovzafar123@gmail.com>
2023-11-10 16:51:55 +0500

    chnage

--- src/components/chat/index.jsx
+++ src/components/chat/index.jsx
@@ -1,11 +1,10 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import { MainContext } from "../..";
-import { Avatar, Button, Container, Grid, TextField } from "@mui/material";
+import { Avatar, Button, Container, Grid } from "@mui/material";
 import Loader from "../loader";
 import firebase from "firebase/compat/app";
-import BackgroundPhoto from "../../assets/background-chat.jpeg";
 
 const Chat = () => {
   const { auth, firestore } = useContext(MainContext);
@@ -23,16 +22,25 @@ const Chat = () => {
     });
     setValue("");
   };
+  const messageRef = useRef();
+  useEffect(() => {
+    if (messageRef.current !== null && typeof window != "undefined") {
+      messageRef?.current?.scrollTo({
+        top: 1000000,
+        behavior: "smooth",
+      });
+    }
+  }, [messages.length]);
   if (loading) return <Loader />;
   return (
-    <Container>
-      <Grid
-        container
-        style={{ height: window.innerHeight - 50, marginTop: 5 }}
-        alignItems={"center"}
-        justifyContent={"center"}
-      >
-        <div className="chat-container">
+    <Grid
+      container
+      style={{ height: window.innerHeight - 50, marginTop: 5 }}
+      alignItems={"center"}
+      justifyContent={"center"}
+    >
+      <div className="chat-container">
+        <div style={{ height: window.innerHeight - 100 }} ref={messageRef}>
           {messages.map((item, index) => (
             <div
               key={index}
@@ -53,47 +61,59 @@ const Chat = () => {
                   width: "40%",
                   padding: 10,
                   borderRadius: 10,
+                  position: "relative",
                 }}
               >
                 <h6>{item.displayName}</h6>
                 <p style={{ marginTop: "10px" }}>{item.text}</p>
+                {/* <span style={{ position: "absolute", right: 10, bottom: 8, fontSize: "10px" }}>
+                  {item.createdAt.seconds}
+                </span> */}
               </div>
             </div>
           ))}
-          <div
-            style={{ display: "flex", width:"100%", position: "absolute", bottom: 0, flexDirection: "row", background: "#ccc", p: 5 }}
-          >
-            <input
-              value={value}
-              onChange={(e) => setValue(e.target.value)}
-              style={{
-                width: "90%",
-                height: 50,
-                margin: "0 10px",
-                border: "none",
-                outline: 0,
-                padding: 10,
-                background: "#ccc",
-              }}
-              placeholder="Enter the send message"
-            />
-            <Button onClick={handleSendMessage}>
-              <svg
-                stroke="currentColor"
-                fill="currentColor"
-                stroke-width="0"
-                viewBox="0 0 512 512"
-                height="1.8em"
-                width="1.8em"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M435.9 64.9l-367.1 160c-6.5 3.1-6.3 12.4.3 15.3l99.3 56.1c5.9 3.3 13.2 2.6 18.3-1.8l195.8-168.8c1.3-1.1 4.4-3.2 5.6-2 1.3 1.3-.7 4.3-1.8 5.6L216.9 320.1c-4.7 5.3-5.4 13.1-1.6 19.1l64.9 104.1c3.2 6.3 12.3 6.2 15.2-.2L447.2 76c3.3-7.2-4.2-14.5-11.3-11.1z"></path>
-              </svg>
-            </Button>
-          </div>
         </div>
-      </Grid>
-    </Container>
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            position: "fixed",
+            bottom: 0,
+            flexDirection: "row",
+            background: "#ccc",
+            p: 5,
+          }}
+        >
+          <input
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            style={{
+              width: "90%",
+              height: 50,
+              margin: "0 10px",
+              border: "none",
+              outline: 0,
+              padding: 10,
+              background: "#ccc",
+            }}
+            placeholder="Enter the send message"
+          />
+          <Button onClick={handleSendMessage}>
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              stroke-width="0"
+              viewBox="0 0 512 512"
+              height="1.8em"
+              width="1.8em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M435.9 64.9l-367.1 160c-6.5 3.1-6.3 12.4.3 15.3l99.3 56.1c5.9 3.3 13.2 2.6 18.3-1.8l195.8-168.8c1.3-1.1 4.4-3.2 5.6-2 1.3 1.3-.7 4.3-1.8 5.6L216.9 320.1c-4.7 5.3-5.4 13.1-1.6 19.1l64.9 104.1c3.2 6.3 12.3 6.2 15.2-.2L447.2 76c3.3-7.2-4.2-14.5-11.3-11.1z"></path>
+            </svg>
+          </Button>
+        </div>
+      </div>
+    </Grid>
   );
 };
 
